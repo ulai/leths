ac15f1b7348c893d16675ac8d0701c859dea6131
ueli <ueli@hotmail.com>
2018-09-03 20:51:37 +0200

    Support for older browsers old.html

--- web/app.js
+++ web/app.js
@@ -1,82 +1,85 @@
-"use strict"
+"use strict";
 
 angular
 .module('app', ['ui.router'])
-.factory('websocket', () => {
-  const socket = io.connect()
-  return socket
+.factory('websocket', function() {
+  var socket = io.connect();
+  return socket;
 })
 //remove leading '#' from color
-.filter('rgb', () => { return function(x) { return x.substr(1) } })
-.config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider) => {
+.filter('rgb', function() { return function(x) { return x.substr(1); } })
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider.state('index', {
     url: '/',
     templateUrl: 'tmpl/index.html',
     controller: ['$scope', 'websocket', function($scope, websocket) {
-      websocket.on('stats', stats => $scope.$apply(() => $scope.stats = stats))
+      websocket.on('stats', function(stats) { $scope.$apply(function() $scope.stats = stats); });
     }]
-  })
+  });
   $stateProvider.state('text', {
     url: '/text',
     templateUrl: 'tmpl/text.html',
     controller: ['$scope', 'websocket', function($scope, websocket) {
-      $scope.set = (k,v) => websocket.emit('set', {k, v})
-      $scope.startScroll = (stepx, stepy, steps, interval, roundoffsets, start) => websocket.emit('startScroll', {stepx, stepy, steps, interval, roundoffsets, start})
-      $scope.stopScroll = () => websocket.emit('stopScroll')
-      $scope.fade = (to, t) => websocket.emit('fade', {to, t})
+      $scope.set = function(k,v) { websocket.emit('set', {k, v}) };
+      $scope.startScroll = function(stepx, stepy, steps, interval, roundoffsets, start) {
+        websocket.emit('startScroll', {stepx, stepy, steps, interval, roundoffsets, start});
+      });
+      $scope.stopScroll = function() { websocket.emit('stopScroll') };
+      $scope.fade = function(to, t) { websocket.emit('fade', {to, t}) };
     }]
-  })
+  });
   $stateProvider.state('list', {
     url: '/list',
     templateUrl: 'tmpl/list.html',
     controller: ['$scope', 'websocket', function($scope, websocket) {
     }]
-  })
+  });
   $stateProvider.state('test', {
     url: '/test',
     templateUrl: 'tmpl/test.html',
     controller: ['$scope', 'websocket', function($scope, websocket) {
-      $scope.fire = i => websocket.emit('fire', i)
-      $scope.tetris = b => websocket.emit('light', {tetris: b})
-      $scope.test = b => websocket.emit('light', {test: b})
-      $scope.noise = b => websocket.emit('light', {noise: b})
-      $scope.wave = b => websocket.emit('light', {wave: b})
-      $scope.clear = b => websocket.emit('light', {clear: 1})
-      $scope.modules = _.times(35, x => {
-        return `rgb(255,255,255)`
+      $scope.fire = function(i) { websocket.emit('fire', i) };
+      $scope.tetris = function(b) { websocket.emit('light', {tetris: b}) };
+      $scope.test = function(b) { websocket.emit('light', {test: b}) };
+      $scope.noise = function(b) { websocket.emit('light', {noise: b}) };
+      $scope.wave = function(b) { websocket.emit('light', {wave: b}) };
+      $scope.clear = function(b) { websocket.emit('light', {clear: 1}) };
+      $scope.modules = _.times(35, function(x) {
+        return 'rgb(255,255,255)';
       })
-      websocket.on('light', l =>
-        $scope.$apply(() => $scope.modules[l.pos.x + l.pos.y * 5] = `rgb(${l.v*255},${l.v*255},${l.v*255})`))
+      websocket.on('light', function(l) {
+        $scope.$apply(function() {$scope.modules[l.pos.x + l.pos.y * 5] = `rgb(${l.v*255},${l.v*255},${l.v*255})`} )
+      })
     }]
   })
   $stateProvider.state('sim', {
     url: '/sim',
     templateUrl: 'tmpl/sim.html',
     controller: ['$scope', 'websocket', function($scope, websocket) {
-      $scope.fire = i => websocket.emit('fire', i)
-      $scope.tetris = b => websocket.emit('light', {tetris: b})
-      $scope.test = b => websocket.emit('light', {test: b})
-      $scope.clear = b => websocket.emit('light', {clear: 1})
+      $scope.fire = function(i) { websocket.emit('fire', i) }
+      $scope.tetris = function(b) { websocket.emit('light', {tetris: b}) }
+      $scope.test = function(b) { websocket.emit('light', {test: b}) }
+      $scope.clear = function(b) { websocket.emit('light', {clear: 1}) }
 
-      const N = 100
+      var N = 100
       $scope.ledchains = {}
-      _.each(['ledchain1', 'ledchain2'], ledchain => {
-        $scope.ledchains[ledchain] = _.times(N, () => `rgb(0,0,0)`)
-        let buffer = new Uint8Array()
-        websocket.on(ledchain, (leds) => $scope.$apply(() => {
-          buffer = new Uint8Array([...buffer, ...(new Uint8Array(leds))])
-          if(buffer.lenght < N * 3) return
-          $scope.ledchains[ledchain] = _.map(_.chunk(new Uint8Array(buffer), 3), x => {
-            return `rgb(${x[0]},${x[1]},${x[2]})`
-          })
-          buffer = new Uint8Array()
-        }))
+      _.each(['ledchain1', 'ledchain2'], function(ledchain) {
+        $scope.ledchains[ledchain] = _.times(N, function() { `rgb(0,0,0)` })
+        var buffer = new Uint8Array()
+        websocket.on(ledchain, function(leds) {
+            $scope.$apply(function() {
+              buffer = new Uint8Array([...buffer, ...(new Uint8Array(leds))])
+              if(buffer.lenght < N * 3) return
+              $scope.ledchains[ledchain] = _.map(_.chunk(new Uint8Array(buffer), 3), function(x) {
+                return `rgb(${x[0]},${x[1]},${x[2]})`
+              })
+              buffer = new Uint8Array()
+        })})
       })
-
     }]
   })
 }])
 .controller('ctrl', ['$scope', '$interval', '$timeout', 'websocket', function($scope, $interval, $timeout, websocket) {
-  websocket.on('stats', stats => $scope.$apply(() => $scope.stats = stats))
+  websocket.on('stats', function(stats) { $scope.$apply(function() { $scope.stats = stats }) })
 }])
